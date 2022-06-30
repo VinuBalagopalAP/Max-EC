4f29da517cd92e4007df9fb622dc4c8bd50aa9a6
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-06-30 23:29:15 +0530

    ⚙️

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -24,7 +24,7 @@ class Category extends Equatable {
           'https://images.unsplash.com/photo-1600718374662-0483d2b9da44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNtb290aGllfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
     ),
     const Category(
-      name: 'Soft Drink',
+      name: 'Water',
       imageUrl:
           'https://images.unsplash.com/photo-1561041695-d2fadf9f318c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0ZXIlMjBib3R0bGVzfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
     ),
